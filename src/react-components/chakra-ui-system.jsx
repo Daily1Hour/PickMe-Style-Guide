de0ea71fe2543b0deee906b1b1e9dfc2c635db04
NarciSource <jwch11@gmail.com>
2025-02-15 14:25:04 +0900

    feat: 색상 시멘틱 함수 추가

--- src/react-components/chakra-ui-system.jsx
+++ src/react-components/chakra-ui-system.jsx
@@ -4,20 +4,16 @@ const config = defineConfig({
     theme: {
         tokens: {
             colors: {
-                pickme: generateColorScale("--pickme-color-primary"),
+                "pickme-primary": generateColorScale("--pickme-color-primary"),
+                "pickme-secondary": generateColorScale("--pickme-color-secondary"),
+                "pickme-tertiary": generateColorScale("--pickme-color-tertiary"),
             },
         },
         semanticTokens: {
             colors: {
-                pickme: {
-                    solid: { value: "{colors.pickme.500}" }, // 기본 색상
-                    contrast: { value: "white" }, // 대비 (글자 등)
-                    fg: { value: "{colors.pickme.700}" }, // 전경색 (글자 등)
-                    muted: { value: "{colors.pickme.300}" }, // 덜 강조된 색상 (버튼/텍스트 등)
-                    subtle: { value: "{colors.pickme.100}" }, // 더 약한 강조 색상 (경계선, 배경 등)
-                    emphasized: { value: "{colors.pickme.700}" }, // 강조 색상 (활성화된 버튼 등)
-                    focusRing: { value: "{colors.pickme.400}" }, // 포커스 효과 색상 (입력 필드 등)
-                },
+                "pickme-primary": generateColorSemantic("pickme-primary"),
+                "pickme-secondary": generateColorSemantic("pickme-secondary"),
+                "pickme-tertiary": generateColorSemantic("pickme-tertiary"),
             },
         },
     },
@@ -40,5 +36,18 @@ function generateColorScale(color) {
         800: `color-mix(in srgb, black 30%, var(${color}))`,
         900: `color-mix(in srgb, black 40%, var(${color}))`,
         950: `color-mix(in srgb, black 50%, var(${color}))`,
+    };
+}
+
+// 색상 시맨틱 토큰 생성 함수
+function generateColorSemantic(color) {
+    return {
+        solid: { value: `{colors.${color}.500}` }, // 기본 색상
+        contrast: { value: "white" }, // 대비 (글자 등)
+        fg: { value: `{colors.${color}.700}` }, // 전경색 (글자 등)
+        muted: { value: `{colors.${color}.300}` }, // 덜 강조된 색상 (버튼/텍스트 등)
+        subtle: { value: `{colors.${color}.100}` }, // 더 약한 강조 색상 (경계선, 배경 등)
+        emphasized: { value: `{colors.${color}.700}` }, // 강조 색상 (활성화된 버튼 등)
+        focusRing: { value: `{colors.${color}.400}` }, // 포커스 효과 색상 (입력 필드 등)
     };
 }
