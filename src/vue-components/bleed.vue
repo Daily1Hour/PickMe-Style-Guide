028971f111285aca3b86ea20fa662fb562f41003
NarciSource <jwch11@gmail.com>
2025-02-16 23:52:45 +0900

    feat: vue용 Bleed 모레큘 컴포넌트 추가

---
+++ src/vue-components/bleed.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="bleed" class="bleed" :style="{ height }">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+const { height } = defineProps<{ height: number }>();
+</script>
+
+<style scoped>
+.bleed {
+    background: #f0f0f0;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,25 @@
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
+        vue(),
         react(),
         federation({
             name: "pickme/style-guide",
             filename: "pickme-style-guide.js",
             exposes: {
                 "./global": "./src/styles/global", // 글로벌 스타일
-                "./react": "./src/react-components", // 아토믹 컴포넌트
+                "./vue": "./src/vue-components", // 뷰 컴포넌트
+                "./react": "./src/react-components", // 리액트 컴포넌트
             },
             shared: {
+                vue: {
+                    requiredVersion: "^3.5.13",
+                },
                 react: {
                     requiredVersion: "^18.3.1",
                 },
