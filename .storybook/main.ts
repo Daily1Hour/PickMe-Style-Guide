0892a7c0c821859655dbea0b74b41a62f20ad07d
NarciSource <jwch11@gmail.com>
2025-03-13 20:03:57 +0900

    fix: PUBLIC_URL이 정의되지 않은 경우 로컬 URL 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,19 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 import { config as dotenvConfig } from "dotenv";
-dotenvConfig();
 
-const PUBLIC_URL = process.env.PUBLIC_URL!;
+dotenvConfig();
+const PUBLIC_URL = process.env.PUBLIC_URL;
 
 const config: StorybookConfig = {
     stories: ["../stories/**/*.mdx", "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
     refs: {
-        react: {
-            title: "React Components",
-            url: PUBLIC_URL + "/react",
-        },
         vue: {
             title: "Vue Components",
-            url: PUBLIC_URL + "/vue",
+            url: PUBLIC_URL ? PUBLIC_URL + "/vue" : "http://localhost:6007",
+        },
+        react: {
+            title: "React Components",
+            url: PUBLIC_URL ? PUBLIC_URL + "/react" : "http://localhost:6008",
         },
     },
     framework: "@storybook/react-vite",
